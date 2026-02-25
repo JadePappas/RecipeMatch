6dbe5c9080eb22592775d2bede8d6dd7fa11737b
Jade Pappas <pappas.293@osu.edu>
2026-02-25 14:46:43 -0500

    add HomeFragment and DiscoverFragment with layouts

---
+++ app/src/main/java/com/example/recipematch/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.recipematch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    // inflate fragment's xml layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    // TODO: Lifecycle logging
+}
